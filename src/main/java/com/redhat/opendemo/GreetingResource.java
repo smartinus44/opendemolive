46aec73eaf90e83644bcaaebdef2f8467a2fc024
Sylvain Martin <symartin@redhat.com>
2023-09-19 08:45:35 +0000

    Quick fix.

--- src/main/java/com/redhat/opendemo/GreetingResource.java
+++ src/main/java/com/redhat/opendemo/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy, from my workstation.";
+        return "Hello RESTEasy, from my workstation, and from online RH devspace.";
     }
 }
